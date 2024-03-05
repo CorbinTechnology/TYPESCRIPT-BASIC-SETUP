6c8fd60dcfa1f6baee1293f86e7c33da26e760a2
CorbinTechnology <techwork@corbintech.co.in>
2024-03-05 12:58:57 +0530

    feat:Added a route for the home for testing the middleware of validateToken

--- src/modules/auth/router/user_routes.ts
+++ src/modules/auth/router/user_routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import UserController from "../controllers/user_controller_class";
+import { validateToken } from "../../../middlewares/auth/checkTokenExpiry ";
 
 
 const user_router=express.Router();
@@ -9,6 +10,7 @@ const user_router=express.Router();
 
 user_router.post("/api/auth/register",UserController.registerUserController);
 user_router.post("/api/auth/login",UserController.loginUserController);
+user_router.post("/home",validateToken,UserController.home);
 
 
 export default user_router;
